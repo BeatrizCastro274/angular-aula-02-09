db04d226f5a7f6abc6d5dca661216118f802f40d
BeatrizCastro274 <111613357+BeatrizCastro274@users.noreply.github.com>
2022-09-02 21:25:40 -0300

    Repetição e pause

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,28 @@ export class AppComponent {
   name = 'Beatriz Castro';
   nascimento = 2000;
   counter = 0;
+  rodada = 0;
   timer = setInterval(() => {
     this.counter = this.counter + 1;
-  });
 
-  getIdade() {
-    return 2022 - this.nascimento;
-  }
+    if (this.rodada < 8) {
+      if (this.counter > 30) {
+        this.counter = 0;
+        this.rodada++;
+      }
+    } else this.counter = 0;
+  }, 10);
+
+  getNumbers() {}
+
+  //getIdade() {
+  //  return 2022 - this.nascimento;
+  //}
+  // getMinutos() {
+  //   return Math.trunc(this.counter / 60);
+  // }
+
+  // getSegundos() {
+  //   return Math.trunc(this.counter % 60);
+  //  }
 }
